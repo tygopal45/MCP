9829cf2bb6d139007cf09f7973fbd02cbe3f2231
tygopal45 <jeegopal835@gmail.com>
2026-09-19 12:08:45 +0530

    Added comments and updated syntax in CH-2 and CH-3

--- CH-3_3rdParty_MCPs/community_mcp.py
+++ CH-3_3rdParty_MCPs/community_mcp.py
@@ -12,7 +12,9 @@ async def main():
     {
         "data_fetch_mcp_stdio":{
             "transport": "stdio",
+            # diretly uses uvx to fetch the duckduckgo-mcp-server which is global 
             "command": "uvx",
+            # argument just after command, which is the name of the MCP server to run
             "args": ["duckduckgo-mcp-server"]
         }
 
@@ -21,16 +23,19 @@ async def main():
 
     # List the tools
     tools = await client.get_tools()
+    # print("Available tools:", len(tools))
+    print(50*'_')
     for tool in tools:
         print("Available tool:", tool.name)
+    
 
     # Call a tool
     # result = await client.invoke("search", {"query": "What is the capital of France?"})
     # print("Tool result:", result)
 
-    fetch_tool = tools[0]
-    result = await fetch_tool.ainvoke({"query": "What is the capital of France?"})
-    print("Tool result:", result)
+    # fetch_tool = tools[0]
+    # result = await fetch_tool.ainvoke({"query": "What is the capital of France?"})
+    # print("Tool result:", result)
 
 
 if __name__ == "__main__":
