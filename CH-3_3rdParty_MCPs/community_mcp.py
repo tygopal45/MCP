90daacf9cc32d1fc5d11cbaeba906a32692a0d19
tygopal45 <jeegopal835@gmail.com>
2026-09-19 12:26:15 +0530

    added .env

--- CH-3_3rdParty_MCPs/community_mcp.py
+++ CH-3_3rdParty_MCPs/community_mcp.py
@@ -24,18 +24,20 @@ async def main():
     # List the tools
     tools = await client.get_tools()
     # print("Available tools:", len(tools))
-    print(50*'_')
     for tool in tools:
         print("Available tool:", tool.name)
     
 
-    # Call a tool
+    # Call a tool (XXXX Not working  Wrong way to call the tool using client.invoke, use tool.ainvoke instead)
     # result = await client.invoke("search", {"query": "What is the capital of France?"})
     # print("Tool result:", result)
 
-    # fetch_tool = tools[0]
-    # result = await fetch_tool.ainvoke({"query": "What is the capital of France?"})
-    # print("Tool result:", result)
+    # Once the client is initialized, you can also directly call the tool using the tool object
+    # Tool object which are Native to LangChain can be used directly to invoke the tool
+
+    fetch_tool = tools[0]
+    result = await fetch_tool.ainvoke({"query": "What is the capital of France?"})
+    print("Tool result:", result)
 
 
 if __name__ == "__main__":
