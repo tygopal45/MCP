8594972c7f5bc0f22e44991e69261b5c87ab35f4
tygopal45 <jeegopal835@gmail.com>
2026-09-19 12:09:16 +0530

    Added comments and updated syntax in CH-2 and CH-3

--- CH-2_HTTP_MCP/2_langchain_client.py
+++ CH-2_HTTP_MCP/2_langchain_client.py
@@ -1,38 +1,38 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
 import asyncio
-import os 
+import os
+import sys
 
-
-# Path to the MCP server script
-mcp_server_script = os.path.join((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),"CH-1_CreateMCP","1_first_mcpserver_stdio.py")
-
-# Path to the virtual environment
-venv_path = os.path.join((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),".venv")
-
+mcp_server_script = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..",
+    "CH-1_CreateMCP",
+    "1_first_mcpserver_stdio.py"
+)
 
 async def main():
 
-    # Create an instance of the MultiServerMCPClient
-    client = MultiServerMCPClient(
-
-    # MCP Server Config (JSON)
-    {
-        "data_fetch_mcp_stdio":{
+    connections = {
+        # STDIO MCP server
+        "data_fetch_mcp_stdio": {
             "transport": "stdio",
-            "command": os.path.join(venv_path, "Scripts", "python.exe"),
-            "args": [str(mcp_server_script)]
+            "command": sys.executable,
+            "args": [mcp_server_script],
         },
-        "data_fetch_mcp_http":{
-            "transport": "streamable-http",
-            "url": "http://localhost:8050/mcp"
-        }
 
+        # Streamable HTTP MCP server -> we added upon the 3_langchain_client.py file to add this connection to the client
+        "data_fetch_mcp_http": {
+            "transport": "streamable_http",
+            "url": "http://localhost:8050/mcp",
+        },
     }
-    )
 
-    # List the tools
+    client = MultiServerMCPClient(connections)
+
     tools = await client.get_tools()
+
     print("Available tools:", tools)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
